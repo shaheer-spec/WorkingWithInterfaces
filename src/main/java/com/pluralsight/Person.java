87b1a8bb46ac6c8e54bf98ca1da2bfec806fbe70
Shaheer kayani <shaheer.kayani8@gmail.com>
2025-10-30 14:42:31 -0400

    Initial Commit with code

--- src/main/java/com/pluralsight/Person.java
+++ src/main/java/com/pluralsight/Person.java
@@ -1,4 +1,35 @@
 package com.pluralsight;
 
-public class Person {
+public class Person implements Comparable<Person>{
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return this.lastName.compareTo(o.lastName);
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + ", " + age;
+    }
 }
